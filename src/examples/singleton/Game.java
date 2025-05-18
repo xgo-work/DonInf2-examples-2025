95fac5dc50a9f3e7ace0b9c9d6179ba81a7be166
xgo-work <71324808+xgo-work@users.noreply.github.com>
2025-05-18 19:51:22 +0200

    complete singleton

--- src/examples/singleton/Game.java
+++ src/examples/singleton/Game.java
@@ -2,8 +2,16 @@ package examples.singleton;
 
 // EXERCICE : faire de cette classe un singleton avec une methode getInstance()
 public class Game {
+    private static Game instance;
 
-    public Game() {
+    private Game() {
 
+    }
+
+    public Game getInstance(){
+        if(Game.instance == null){
+            Game.instance = new Game();
+        }
+        return Game.instance;
     }
 }
